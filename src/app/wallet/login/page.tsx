cbf27f57ae754a6fd890ba6816f3763f1a0ae03d
geraldngjx <geraldngjx@gmail.com>
2024-06-29 02:56:05 +0800

    Add wallet pages

---
+++ src/app/wallet/login/page.tsx
@@ -0,0 +1,8 @@
+// Might not require this page - lowest priority (if user log into tiktok - considered logged in)
+export default function WalletLogin() {
+  return (
+    <div>
+      <h1>Login</h1>
+    </div>
+  );
+}
---
+++ src/app/wallet/page.tsx
@@ -0,0 +1,8 @@
+// This page routes the user to respective wallet pages.
+export default function WalletRoute() {
+  return (
+    <div>
+      <h1>WalletRoute</h1>
+    </div>
+  );
+}
