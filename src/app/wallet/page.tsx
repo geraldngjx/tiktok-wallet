c0b1cd6dbc9b1d40ace48d64f15dee82b4b5307e
9teMare <wang.zihao@u.nus.edu>
2024-07-02 19:59:13 +0800

    Update page.tsx

--- src/app/wallet/page.tsx
+++ src/app/wallet/page.tsx
@@ -1,28 +1,19 @@
 "use client";
-import { Button, Card } from "@mui/material";
-import Balance from "@/components/wallet/balance";
-import TransactionList from "@/components/wallet/transactionList";
-import { AccountCircle, QrCodeScanner, Send } from "@mui/icons-material";
-import { signout } from "@/lib/auth";
-import Link from "next/link";
-import { useEffect, useState } from "react";
-import WalletHome from "./wallet-dashboard";
-import Login from "./login/login";
 import { useMagicTokenStore } from "@/store/magicTokenStore";
+import { useEffect } from "react";
+import Login from "./login/login";
+import WalletHome from "./wallet-dashboard";
 
 export default function Wallet() {
     const { token, setToken } = useMagicTokenStore();
-    // const [token, setToken] = useState("");
 
     useEffect(() => {
         setToken(localStorage.getItem("token") ?? "");
     }, [setToken]);
 
     return (
-        <div className="h-full flex w-full items-center justify-center overflow-hidden" vaul-drawer-wrapper="">
+        <div className="h-full flex w-full items-center justify-center bg-[#010101] overflow-hidden" vaul-drawer-wrapper="">
             {process.env.NEXT_PUBLIC_MAGIC_API_KEY ? token.length > 0 ? <WalletHome /> : <Login /> : <div>Missing Magic API Key</div>}
         </div>
     );
 }
-
-// export default withAuth(WalletHome);
