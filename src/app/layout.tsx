8743ad3a8b2d16f57d27aa8e25b21fae3304bc1a
geraldngjx <geraldngjx@gmail.com>
2024-07-06 11:52:33 +0800

    Update metadata content

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,9 +7,9 @@ import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import Head from "next/head";
 
-const APP_NAME = "TikTok Wallet";
-const APP_DEFAULT_TITLE = "My TikTok Wallet";
-const APP_TITLE_TEMPLATE = "%s - TikTok Wallet";
+const APP_NAME = "ByteSecure";
+const APP_DEFAULT_TITLE = "ByteSecure";
+const APP_TITLE_TEMPLATE = "%s - ByteSecure";
 const APP_DESCRIPTION = "Your trusted and secure wallet application.";
 
 const fontSans = FontSans({
@@ -43,11 +43,14 @@ export default function RootLayout({
           name="viewport"
           content="width=device-width, initial-scale=1, viewport-fit=cover"
         />
-        <meta name="application-name" content="PWA App" />
+        <meta name="application-name" content="ByteSecure" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="apple-mobile-web-app-title" content="PWA App" />
-        <meta name="description" content="Best PWA App in the world" />
+        <meta name="apple-mobile-web-app-title" content="ByteSecure" />
+        <meta
+          name="description"
+          content="Your trusted and secure wallet application."
+        />
         <meta name="format-detection" content="telephone=no" />
         <meta name="mobile-web-app-capable" content="yes" />
         <meta name="msapplication-config" content="/icons/browserconfig.xml" />
@@ -55,57 +58,6 @@ export default function RootLayout({
         <meta name="msapplication-tap-highlight" content="no" />
         <meta name="theme-color" content="#000000" />
 
-        <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
-        <link
-          rel="apple-touch-icon"
-          sizes="152x152"
-          href="/icons/touch-icon-ipad.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="180x180"
-          href="/icons/touch-icon-iphone-retina.png"
-        />
-        <link
-          rel="apple-touch-icon"
-          sizes="167x167"
-          href="/icons/touch-icon-ipad-retina.png"
-        />
-
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="32x32"
-          href="/icons/favicon-32x32.png"
-        />
-        <link
-          rel="icon"
-          type="image/png"
-          sizes="16x16"
-          href="/icons/favicon-16x16.png"
-        />
-        <link rel="manifest" href="/manifest.json" />
-        <link
-          rel="mask-icon"
-          href="/icons/safari-pinned-tab.svg"
-          color="#5bbad5"
-        />
-        <link rel="shortcut icon" href="/favicon.ico" />
-
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:url" content="https://yourdomain.com" />
-        <meta name="twitter:title" content="PWA App" />
-        <meta name="twitter:description" content="Best PWA App in the world" />
-        <meta
-          name="twitter:image"
-          content="https://yourdomain.com/icons/android-chrome-192x192.png"
-        />
-        <meta name="twitter:creator" content="@DavidWShadow" />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content="PWA App" />
-        <meta property="og:description" content="Best PWA App in the world" />
-        <meta property="og:site_name" content="PWA App" />
-        <meta property="og:url" content="https://yourdomain.com" />
         <meta
           property="og:image"
           content="https://yourdomain.com/icons/apple-touch-icon.png"
