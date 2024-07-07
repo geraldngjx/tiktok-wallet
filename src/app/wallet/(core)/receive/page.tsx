1982afe700ed26dd83a95cc99cce2e5b22459fce
9teMare <wang.zihao@u.nus.edu>
2024-07-07 23:25:54 +0800

    Update page.tsx

--- src/app/wallet/(core)/receive/page.tsx
+++ src/app/wallet/(core)/receive/page.tsx
@@ -20,148 +20,118 @@ import dayjs from "dayjs";
 import { CURRENCY } from "@/utils/types/currency";
 
 function Page() {
-  const [open, setOpen] = useState(false);
+    const [open, setOpen] = useState(false);
 
-  const { data: magicUser, status } = useQuery({
-    ...useMagicUserQuery(),
-    staleTime: 0,
-  });
+    const { data: magicUser, status } = useQuery({
+        ...useMagicUserQuery(),
+        staleTime: 0,
+    });
 
-  const [time, setTime] = useState(dayjs());
+    const [time, setTime] = useState(dayjs());
 
-  const [amount, setAmount] = useState("");
-  const [acceptedCurrency, setAcceptedCurrency] = useState(CURRENCY.SOLANA);
+    const [amount, setAmount] = useState("");
+    const [acceptedCurrency, setAcceptedCurrency] = useState(CURRENCY.SOLANA);
 
-  const [qrCodeValue, setQrCodeValue] = useState("");
+    const [qrCodeValue, setQrCodeValue] = useState("");
 
-  useEffect(() => {
-    if (status === "success" && magicUser) {
-      setQrCodeValue(
-        encodeURIComponent(
-          `byteSecure=true&email=${
-            magicUser.email
-          }&time=${new Date().getTime()}`.replaceAll("+", "%2B")
-        )
-      );
+    useEffect(() => {
+        if (status === "success" && magicUser) {
+            setQrCodeValue(encodeURIComponent(`byteSecure=true&email=${magicUser.email}&time=${new Date().getTime()}`.replaceAll("+", "%2B")));
 
-      setTime(dayjs());
-    }
-  }, [magicUser, status]);
+            setTime(dayjs());
+        }
+    }, [magicUser, status]);
 
-  return (
-    <div
-      className="flex flex-col w-full h-full space-y-32 justify-center items-center p-4"
-      vaul-drawer-wrapper=""
-    >
-      {status !== "success" ? (
-        <Disc3Icon className="animate-spin w-10 h-10" />
-      ) : (
-        <>
-          <ShineBorder
-            className="text-center min-w-10 min-h-10 size-52 p-2"
-            color={["#A07CFE", "#FE8FB5", "#FFBE7B"]}
-            borderWidth={4}
-            borderRadius={2}
-          >
-            <QRCode
-              style={{ height: "auto", maxWidth: "100%", width: "100%" }}
-              value={qrCodeValue}
-              viewBox={`0 0 256 256`}
-            />
-          </ShineBorder>
-
-          <p className="text-center text-slate-700">
-            This QR code is valid until{" "}
-            {time.add(10, "minute").format("DD MMM YYYY, H:mm:ss A")}
-          </p>
-
-          <Drawer
-            open={open}
-            onOpenChange={(open) => {
-              if (!open) {
-                setAmount("");
-                setAcceptedCurrency(CURRENCY.SOLANA);
-              }
-              setOpen(open);
-            }}
-          >
-            <DrawerTrigger asChild>
-              <Button className="rounded-full">Request payment</Button>
-            </DrawerTrigger>
-
-            <DrawerContent className="px-6 h-fit min-h-[40vh] bg-neutral-900 border-0">
-              <DialogTitle className="hidden">Configure</DialogTitle>
-              <DialogDescription className="hidden" />
-
-              <div className="flex w-full h-[50vh] flex-col justify-between items-center px-20 py-10">
-                <div className="space-y-2">
-                  <Label className="text-lg ml-1 font-medium text-slate-500">
-                    Accepted currency:
-                  </Label>
-                  <ToggleGroup
-                    type="single"
-                    className="space-x-2"
-                    defaultValue={CURRENCY.USDC}
-                    value={acceptedCurrency}
-                    onValueChange={(value: CURRENCY) =>
-                      setAcceptedCurrency(value)
-                    }
-                  >
-                    <ToggleGroupItem
-                      className="rounded-full pl-1 pr-2"
-                      value={CURRENCY.SOLANA}
+    return (
+        <div className="flex flex-col w-full h-full space-y-32 justify-center items-center p-4" vaul-drawer-wrapper="">
+            {status !== "success" ? (
+                <Disc3Icon className="animate-spin w-10 h-10" />
+            ) : (
+                <>
+                    <ShineBorder
+                        className="text-center min-w-10 min-h-10 size-52 p-2"
+                        color={["#A07CFE", "#FE8FB5", "#FFBE7B"]}
+                        borderWidth={4}
+                        borderRadius={2}
                     >
-                      {getIconByCurrency("Solana")}Solana
-                    </ToggleGroupItem>
-                    <ToggleGroupItem
-                      className="rounded-full pl-0 pr-2"
-                      value={CURRENCY.USDC}
-                    >
-                      {getIconByCurrency("USDC")}USDC
-                    </ToggleGroupItem>
-                    <ToggleGroupItem
-                      className="rounded-full pl-0 pr-2"
-                      value={CURRENCY.EURC}
-                    >
-                      {getIconByCurrency("EURC")}EURC
-                    </ToggleGroupItem>
-                  </ToggleGroup>
-                </div>
+                        <QRCode style={{ height: "auto", maxWidth: "100%", width: "100%" }} value={qrCodeValue} viewBox={`0 0 256 256`} />
+                    </ShineBorder>
 
-                <Input
-                  placeholder="0.00"
-                  type="number"
-                  className="h-14 w-40 text-6xl font-bold text-center border-0 focus-visible:ring-0 focus-visible:placeholder:opacity-0 caret-slate-500 px-0"
-                  onChange={(e) => setAmount(e.target.value)}
-                />
+                    <p className="text-center text-slate-700">
+                        This QR code is valid until {time.add(10, "minute").format("DD MMM YYYY, H:mm:ss A")}
+                    </p>
 
-                <Button
-                  className="rounded-full w-full"
-                  disabled={amount === "" || !acceptedCurrency}
-                  onClick={() => {
-                    setOpen(false);
-                    setTime(dayjs());
-                    setQrCodeValue(
-                      encodeURIComponent(
-                        `byteSecure=true&email=${
-                          magicUser?.email
-                        }&amount=${amount}&currency=${acceptedCurrency}&time=${new Date().getTime()}`.replaceAll(
-                          "+",
-                          "%2B"
-                        )
-                      )
-                    );
-                  }}
-                >
-                  Regenerate QR code
-                </Button>
-              </div>
-            </DrawerContent>
-          </Drawer>
-        </>
-      )}
-    </div>
-  );
+                    <Drawer
+                        open={open}
+                        onOpenChange={(open) => {
+                            if (!open) {
+                                setAmount("");
+                                setAcceptedCurrency(CURRENCY.SOLANA);
+                            }
+                            setOpen(open);
+                        }}
+                    >
+                        <DrawerTrigger asChild>
+                            <Button className="rounded-full">Request payment</Button>
+                        </DrawerTrigger>
+
+                        <DrawerContent className="px-6 h-fit min-h-[40vh] bg-neutral-900 border-0">
+                            <DialogTitle className="hidden">Configure</DialogTitle>
+                            <DialogDescription className="hidden" />
+
+                            <div className="flex w-full h-[50vh] flex-col justify-between items-center px-20 py-10">
+                                <div className="space-y-2">
+                                    <Label className="text-lg ml-1 font-medium text-slate-500">Accepted currency:</Label>
+                                    <ToggleGroup
+                                        type="single"
+                                        className="space-x-2"
+                                        defaultValue={CURRENCY.USDC}
+                                        value={acceptedCurrency}
+                                        onValueChange={(value: CURRENCY) => setAcceptedCurrency(value)}
+                                    >
+                                        <ToggleGroupItem className="rounded-full pl-1 pr-2" value={CURRENCY.SOLANA}>
+                                            {getIconByCurrency("Solana")}Solana
+                                        </ToggleGroupItem>
+                                        <ToggleGroupItem className="rounded-full pl-0 pr-2" value={CURRENCY.USDC}>
+                                            {getIconByCurrency("USDC")}USDC
+                                        </ToggleGroupItem>
+                                        <ToggleGroupItem className="rounded-full pl-0 pr-2" value={CURRENCY.EURC}>
+                                            {getIconByCurrency("EURC")}EURC
+                                        </ToggleGroupItem>
+                                    </ToggleGroup>
+                                </div>
+
+                                <Input
+                                    placeholder="0.00"
+                                    type="number"
+                                    className="h-14 w-40 text-6xl font-bold text-center border-0 focus-visible:ring-0 focus-visible:placeholder:opacity-0 caret-slate-500 px-0"
+                                    onChange={(e) => setAmount(e.target.value)}
+                                />
+
+                                <Button
+                                    className="rounded-full w-full"
+                                    disabled={amount === "" || !acceptedCurrency}
+                                    onClick={() => {
+                                        setOpen(false);
+                                        setTime(dayjs());
+                                        setQrCodeValue(
+                                            encodeURIComponent(
+                                                `byteSecure=true&email=${
+                                                    magicUser?.email
+                                                }&amount=${amount}&currency=${acceptedCurrency}&time=${new Date().getTime()}`.replaceAll("+", "%2B")
+                                            )
+                                        );
+                                    }}
+                                >
+                                    Regenerate QR code
+                                </Button>
+                            </div>
+                        </DrawerContent>
+                    </Drawer>
+                </>
+            )}
+        </div>
+    );
 }
 
 export default withAuthMagic(Page);
